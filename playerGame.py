7976062ce772fc800df9a1161b0e20b3f331e2b3
nsp456 <61820335+nsp456@users.noreply.github.com>
2020-06-02 20:06:52 +0530

    Update playerGame.py

--- playerGame.py
+++ playerGame.py
@@ -1,17 +1,9 @@
-import sys, pygame,math,time
+import sys, pygame,math,time,random
 pygame.init()
 
-gameicon = pygame.image.load('PlayerGame/goal.png')
-pygame.display.set_icon(gameicon)
-window_width = 900
-window_height = 650
-gameWindow = pygame.display.set_mode((window_width, window_height))
-pygame.display.set_caption("Player Game")
-pygame.display.update()
-clock = pygame.time.Clock()
-font = pygame.font.SysFont("consolas", 25)
-
-
+def distance(pos,goal):
+    dist=math.sqrt((pos[0]-goal[0])**2 +(pos[1]-goal[1])**2)
+    return dist*100
 #-----------------Init Graphics-----------------------------
 size = width, height = 700, 500
 speed = 5
@@ -30,23 +22,25 @@ og_player = pygame.image.load("PlayerGame/player.png")
 player=og_player.copy()
 #player=pygame.transform.rotate(player, -180)
 playerrect = player.get_rect()
-playerrect=playerrect.move([50,325])
+playerrect=playerrect.move([55,325])
 
 #--------------------------Init Logging Variables------------------------------
 trials=0
 min_ind=-1
 feedback=[]
-for i in range (height):
+prev_choice=[]
+for i in range (width):
     temp=[]
-    for j in range (width):
+    for j in range (height):
         temp.append(0)
     feedback.append(temp)
-#choice=[]
+
 #---------------------------------------Init Physics-------------------------------------------
 g=1
-jump=0
+
 direction=0
 flag=0
+sleeper=0
 #--------------------------------------Loop of Trials---------------------------------------------------
 while 1: 
     for event in pygame.event.get():
@@ -54,62 +48,121 @@ while 1:
     trials+=1
     i=-1
     print("Trial Number : "+str(trials))
-    
+    font = pygame.font.Font('freesansbold.ttf', 32) 
+    text = font.render(str(trials), True, [0,0,0], [255,255,255])
+    text_rect=text.get_rect()
+    ai_event=0
+    jump=0
+    flag=0
+    choice=[]
     if(trials!=1):
         
         playerrect = player.get_rect()
-        playerrect=playerrect.move([50,325])
+        playerrect=playerrect.move([55,325])
         
         
     #------------------------------------Game Loop-------------------------------------------------
     while 1:
+        ai_event=0
+        quit_flag=0
         direction=0
-        bottom_sensor=[int(playerrect.centerx),int(playerrect.centery)+35]
+        bottom_sensor=[int(playerrect.centerx),int(playerrect.centery)+30]
         left_sensor=[int(playerrect.centerx- 35),int(playerrect.centery)]
         right_sensor=[int(playerrect.centerx+35),int(playerrect.centery)]
         up_sensor=[int(playerrect.centerx),int(playerrect.centery)-20]
         events = pygame.event.get()
+        left_feed=feedback[int(playerrect.centerx- 5)][int(playerrect.centery+g - jump)]
+        right_feed=feedback[int(playerrect.centerx+5)][int(playerrect.centery+g - jump)]
         
+        if(level.get_at(bottom_sensor)==(0,0,0,255)):
+            jump_feed=feedback[up_sensor[0]][up_sensor[1]]
+        else:
+            jump_feed=feedback[int(playerrect.centerx)][int(playerrect.centery + g - jump)]
+        #print(jump_feed," ",right_feed," ",left_feed)
+        if(right_feed==left_feed and left_feed==jump_feed): #all 3 equal
+            #print("hey")
+            ai_event=random.choice([1,2,3])
+        #--------------------------2 equal and greater than 3rd-------------
+        elif(right_feed==left_feed and right_feed>jump_feed):
+            ai_event=random.choice([1,2])
+        elif(right_feed==jump_feed and right_feed>left_feed):
+            ai_event=random.choice([2,3])
+        elif(jump_feed==left_feed and left_feed>right_feed):
+            ai_event=random.choice([1,3])
+        #--------------------------------------------------------------
+        #--------------------1 greatest------------------------------------
+        elif(right_feed>left_feed and right_feed>jump_feed):
+            ai_event=2
+        elif(left_feed>jump_feed and left_feed>right_feed):
+            ai_event=1
+        elif(jump_feed>right_feed and jump_feed>left_feed):
+            ai_event=3
+        #print(ai_event," ",left_feed," ",jump_feed," ",right_feed)
+        """
         for event in events:
-            if event.type == pygame.KEYDOWN:
+            if event.type == pygame.KEYDOWN: 
                 #if event.key == pygame.K_LEFT:
                    # direction = -1
                 #if event.key == pygame.K_RIGHT:
                     #direction = 1
-                if event.key == pygame.K_SPACE:
+                if event.key == pygame.K_SPACE :
                    if(level.get_at(bottom_sensor)==(0,0,0,255)): 
                         jump=20
-       
+        """
+        if ai_event==3:
+            if(level.get_at(bottom_sensor)==(0,0,0,255)): 
+                jump=20
         keys=pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
+        if ai_event==1:
             if(left_sensor[0]>0):
                 direction=-1
-        if keys[pygame.K_RIGHT]:
+            else:
+                quit_flag=1
+        if ai_event==2:
             if(right_sensor[0]<width):
                 direction=1
+            else:
+                quit_flag=1
         
         if(level.get_at(bottom_sensor)!=(0,0,0,255)):
             g=2
         else:
             g=0
-        speed=[direction*3,g-jump]
+        speed=[direction*5,g-jump]
         if(jump>0):
             jump-=g
+        original_pos=playerrect.center
         playerrect=playerrect.move(speed)
-        
+        #-----------------------------------------
+        feedback[playerrect.centerx][playerrect.centery]+=(distance(original_pos,goalrect.center)-distance(playerrect.center,goalrect.center))
+        choice.append([playerrect.centerx,playerrect.centery])
+        if(quit_flag==1):
+            break
+        #print(feedback[playerrect.centerx][playerrect.centery])
+        #----------------------------------------------
         #screen.fill("level.png")
         screen.blit(level,[0,0])
         screen.blit(goal,goalrect)
         screen.blit(player, playerrect)
+        screen.blit(text,text_rect)
         if(playerrect.collidepoint([goalrect.centerx,goalrect.centery])):
             screen.blit(victory,[250,200])
             flag=1
         pygame.display.flip()
-        time.sleep(0.01)
+        time.sleep(sleeper)
         if(flag==1):
+                for coord in choice:
+                    feedback[coord[0]][coord[1]]*=2
                 time.sleep(1)
-                flag=0
+                #flag=0
+                sleeper=0.0001
                 break
     #--------------------------------End Of Game Loop----------------------------
+    if choice==prev_choice and flag!=1:
+        for coord in choice:
+                    feedback[coord[0]][coord[1]]/=2
+        
+    prev_choice=choice
     #write_list(feedback,choice)
 #----------------------------------------End of All Trials-----------------------------------------------------    
+ 
